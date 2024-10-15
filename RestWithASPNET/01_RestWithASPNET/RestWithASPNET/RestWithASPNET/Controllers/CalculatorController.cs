faf9ca347adb2e7a564a3e95ef0b7ee277d971d8
Claudio Matheus <claudiomatheus055@gmail.com>
2024-10-14 22:06:58 -0300

    Implement IsNumeric and convertToDecimal methods

--- RestWithASPNET/01_RestWithASPNET/RestWithASPNET/RestWithASPNET/Controllers/CalculatorController.cs
+++ RestWithASPNET/01_RestWithASPNET/RestWithASPNET/RestWithASPNET/Controllers/CalculatorController.cs
@@ -6,7 +6,7 @@ namespace RestWithASPNET.Controllers
     [Route("[controller]")]
     public class CalculatorController : ControllerBase
     {
-       
+
         private readonly ILogger<CalculatorController> _logger;
 
         public CalculatorController(ILogger<CalculatorController> logger)
@@ -24,17 +24,31 @@ namespace RestWithASPNET.Controllers
             }
 
 
-            return BadRequest("Invalid Input");   
+            return BadRequest("Invalid Input");
         }
 
-        private int ConvertToDecimal(string firstNumber)
+        private decimal ConvertToDecimal(string strNumber)
         {
-            throw new NotImplementedException();
+            decimal decimalValue;
+            if(decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
         }
 
-        private bool IsNumeric(string firstNumber)
+        private bool IsNumeric(string strNumber)
         {
-            throw new NotImplementedException();
+            double number;
+
+            bool IsNumber = double.TryParse(strNumber, 
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out number);
+
+            return IsNumber;
+
         }
     }
 }
