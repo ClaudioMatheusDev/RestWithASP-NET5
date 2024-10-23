e75baaca99b6be4ae765778a32ffda6d6de15a39
Claudio Matheus <claudiomatheus055@gmail.com>
2024-10-23 17:56:53 -0300

    Removing remaining mocks

--- RestWithASPNET/01_RestWithASPNET/RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
+++ RestWithASPNET/01_RestWithASPNET/RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -1,5 +1,9 @@
-﻿using RestWithASPNET.Model;
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using RestWithASPNET.Model;
 using RestWithASPNET.Model.Context;
+using System;
+using System.Diagnostics;
 
 namespace RestWithASPNET.Services.Implementations
 {
@@ -8,6 +12,17 @@ namespace RestWithASPNET.Services.Implementations
         private MySQLContext _context;
 
 
+        public List<Person> FindAll()
+        {
+
+            return _context.Persons.ToList();
+        }//FIND ALL
+
+        public Person FindByID(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }//FIND BY ID
+
         public PersonServiceImplementation(MySQLContext context)
         {
             _context = context;
@@ -15,38 +30,66 @@ namespace RestWithASPNET.Services.Implementations
 
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
             return person;
-        }
+        }//CREATE 
 
         public void Delete(long id)
         {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
 
-        }
-
-        public List<Person> FindAll()
-        {
-            
-            return _context.Persons.ToList();
-        }
-
-        public Person FindByID(long id)
-        {
-            return new Person
+            if (result != null)
             {
-                Id = 1,
-                FirstName = "Claudio",
-                LastName = "Matheus",
-                Addres = "Adamantina - São Paulo - Brasil",
-                Gender = "Male"
-            };
-        }
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+        }//DELETE
+
 
         public Person Update(Person person)
         {
+
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+
             return person;
+        }// UPDATE
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
-
-
- 
     }
 }
